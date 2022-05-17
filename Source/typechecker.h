bbb26e6a2fd3477c32caad7ee7bb7503899220ea
Tyler Jaacks <tjaacks@iastate.edu>
2022-05-17 23:00:24 +0000

    Update Source/typechecker.h

--- Source/typechecker.h
+++ Source/typechecker.h
@@ -1,4 +1,6 @@
+#include <algorithm>
 #include <map>
+#include <memory>
 #include <vector>
 
 #include "AST.h"
@@ -343,7 +345,7 @@ private:
     template<typename ... Args>
     std::string string_format(const std::string &format, Args ... args) {
         int size_s = std::snprintf(nullptr, 0, format.c_str(), args ...) + 1; // Extra space for '\0'
-        if (size_s <= 0) { throw std::runtime_error("Error during formatting."); }
+        if (size_s <= 0) { } // throw std::runtime_error("Error during formatting."); }
         auto size = static_cast<size_t>( size_s );
         std::shared_ptr<char[]> buf(new char[size]);
         std::snprintf(buf.get(), size, format.c_str(), args ...);
